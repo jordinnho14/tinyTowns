ea04c3304f096a393ed6b7ade183280321a6b34b
Jordinnho14 <jordan_thompson14@hotmail.co.uk>
2022-09-06 22:26:06 +0100

    setting up app

---
+++ tiny-towns/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <div>Tiny Towns!</div>
+    )
+  }
+}
+
+export default App;
