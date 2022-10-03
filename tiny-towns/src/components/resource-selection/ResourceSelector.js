8acdd00a33562d2d72015101d270a87b6f0dcc8f
Jordan Thompson <JThompson@scottlogic.com>
2022-10-03 16:04:32 +0100

    adds other building cards to game

--- tiny-towns/src/components/resource-selection/ResourceSelector.js
+++ tiny-towns/src/components/resource-selection/ResourceSelector.js
@@ -1,15 +1,15 @@
 import React from 'react';
-
+import './resourceSelector.css';
 function ResourceSelector (props) {
     
     return (
         <div>
             <div>Select next resource</div>
-            <button onClick={() => props.handleSelectedResourceChange('wood')}>Wood</button>
-            <button onClick={() => props.handleSelectedResourceChange('stone')}>Stone</button>
-            <button onClick={() => props.handleSelectedResourceChange('glass')}>Glass</button>
-            <button onClick={() => props.handleSelectedResourceChange('brick')}>Brick</button>
-            <button onClick={() => props.handleSelectedResourceChange('wheat')}>Wheat</button>
+            <button className="wood-selection-button" onClick={() => props.handleSelectedResourceChange('wood')}>Wood</button>
+            <button className="stone-selection-button" onClick={() => props.handleSelectedResourceChange('stone')}>Stone</button>
+            <button className="glass-selection-button" onClick={() => props.handleSelectedResourceChange('glass')}>Glass</button>
+            <button className="brick-selection-button" onClick={() => props.handleSelectedResourceChange('brick')}>Brick</button>
+            <button className="wheat-selection-button" onClick={() => props.handleSelectedResourceChange('wheat')}>Wheat</button>
         </div>
     )
 }
